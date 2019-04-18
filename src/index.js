727987ffc75f3d68b7553e80857d69c35538ed9f
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2019-04-17 17:58:53 -0700

    First Commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const App = () => {
+    return <div>Hello React, Webpack 4 &amp; Babel 7! It finally works</div>
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"))
